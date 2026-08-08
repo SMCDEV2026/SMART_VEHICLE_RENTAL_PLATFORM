41d3586297a2fb1b0c7f2ddfa97cbb3f3972b1ea
SMCDEV2026 <smctech2027@gmail.com>
2026-08-08 14:51:33 +0530

    Restrict CORS to production URL
    Updated CORS middleware to restrict allowed origins to the production URL.

--- BACKEND/app/main.py
+++ BACKEND/app/main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 # ====================================================
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Change in production
+    allow_origins=["https://smart-vehicle-rental-platform-ten.vercel.app"],  # Change in production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
